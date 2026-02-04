5591710047e08fa7660b93348c8713eb5121177b
Diego Lobo <diego.lobo@refuturiza.com.br>
2026-02-03 23:42:18 -0300

    feat: Criação do DTO de Paginação.

---
+++ src/main/java/br/com/rockstars/domain/dto/PageResponseDTO.java
@@ -0,0 +1,87 @@
+package br.com.rockstars.domain.dto;
+
+import java.util.List;
+
+public class PageResponseDTO<T> {
+
+    private List<T> content;
+    private int page;
+    private int size;
+    private long totalElements;
+    private int totalPages;
+    private boolean first;
+    private boolean last;
+
+    public PageResponseDTO() {
+    }
+
+    public PageResponseDTO(List<T> content, int page, int size, long totalElements) {
+        this.content = content;
+        this.page = page;
+        this.size = size;
+        this.totalElements = totalElements;
+        this.totalPages = size > 0 ? (int) Math.ceil((double) totalElements / size) : 0;
+        this.first = page == 0;
+        this.last = page >= totalPages - 1;
+    }
+
+    public static <T> PageResponseDTO<T> of(List<T> content, int page, int size, long totalElements) {
+        return new PageResponseDTO<>(content, page, size, totalElements);
+    }
+
+    public List<T> getContent() {
+        return content;
+    }
+
+    public void setContent(List<T> content) {
+        this.content = content;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public long getTotalElements() {
+        return totalElements;
+    }
+
+    public void setTotalElements(long totalElements) {
+        this.totalElements = totalElements;
+    }
+
+    public int getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(int totalPages) {
+        this.totalPages = totalPages;
+    }
+
+    public boolean isFirst() {
+        return first;
+    }
+
+    public void setFirst(boolean first) {
+        this.first = first;
+    }
+
+    public boolean isLast() {
+        return last;
+    }
+
+    public void setLast(boolean last) {
+        this.last = last;
+    }
+}
